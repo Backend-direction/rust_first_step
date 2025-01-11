02a1a242c09f58b7cc331cc9aed562ab6ad3ec6a
Volodymyr Pankiv <vpanki@softserveinc.com>
2025-01-11 09:11:21 +0200

    added shuffle
    Signed-off-by: Volodymyr Pankiv <vpanki@softserveinc.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::{thread_rng, seq::SliceRandom};
+
 #[derive(Debug)]
 
 struct Deck {
@@ -24,10 +26,26 @@ impl Deck {
 
     return Deck { cards };
   }
+
+  fn shuffle(&mut self) {
+    let mut rng = thread_rng();
+
+    self.cards.shuffle(&mut rng);
+  }
+
+  fn deal(&mut self, num_cards: usize)-> Vec<String> {
+    return self.cards.split_off(
+      self.cards.len() - num_cards
+    );
+  }
 }
 
 fn main() {
-  let deck = Deck::new();
+  let mut deck = Deck::new();
 
-  println!("Here is your deck: {:#?}", deck);
+  // deck.shuffle();
+
+  let cards = deck.deal(3);
+
+  println!("Here is your deck: {:#?}", cards);
 }
